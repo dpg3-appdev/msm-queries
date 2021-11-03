66e9eba470dbf4f3772e5c6e3421b8ed8b3ec0c2
dpg3 <dguarin0@chicagobooth.edu>
2021-11-03 03:23:27 +0000

    Got Directors Mostly Done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -19,6 +19,12 @@ class DirectorsController < ApplicationController
   end
 
   def director_details
+    #params looks like this {"an_id"=>"42"}
+
+    the_id = params.fetch("an_id")
+
+    @the_director = Director.where({ :id => the_id }).at(0)
+
     render({ :template => "director_templates/show.html.erb"})
   end
 
