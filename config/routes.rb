4f43810e8aa1f4e424ef857830e34d24f774d063
dpg3 <dguarin0@chicagobooth.edu>
2021-11-05 16:47:07 +0000

    Directors Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  
+  
   get("/directors/eldest", { :controller => "directors", :action => "wisest"})
   get("/directors/youngest", { :controller => "directors", :action => "foolish"})
   get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
 
+  get("/movies/:an_id", { :controller => "movies", :action => "movie_details" })
+
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors", { :controller => "directors", :action => "index" })
+  get("/movies", { :controller => "movies", :action => "index" })
+
 end
