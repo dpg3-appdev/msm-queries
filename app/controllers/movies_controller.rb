b38918c815f70ff1ab6320fc3cccb1685625e008
dpg3 <dguarin0@chicagobooth.edu>
2021-11-05 17:35:05 +0000

    Kicking ass and chewing gum Actor Left

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
 
   def index
-    @list_of_movies = Movies.all
+    @list_of_movies = Movie.all
 
     render({ :template => "movie_templates/index.html.erb"})
   end
@@ -12,7 +12,8 @@ class MoviesController < ApplicationController
 
      the_id = params.fetch("an_id")
 
-     @the_movie = Movies.where({ :id => the_id }).at(0)
+     @the_movie = Movie.where({ :id => the_id }).at(0)
  
      render({ :template => "movie_templates/show.html.erb"})   
   end
+end
